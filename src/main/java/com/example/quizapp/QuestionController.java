67049dc8a749462db55b1aefce0b9019cc1f1004
Sherif-toiema22 <sherif.toiema22@gmail.com>
2025-06-01 11:59:12 +0300

    Initial commit of Spring Boot project

---
+++ src/main/java/com/example/quizapp/QuestionController.java
@@ -0,0 +1,14 @@
+package com.example.quizapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+    @GetMapping("allquestions")
+    public String allquestions() {
+        return "All questions";
+    }
+}
